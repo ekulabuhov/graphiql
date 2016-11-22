017b1dd6e781fb40d919168e966603c9bfed04ca
Jevgenijs Kulabuhovs <jevgenijs.kulabuhovs@logograb.com>
2016-11-22 14:05:24 +0000

    🙄 Apparently dist is the only folder I need

---
+++ dist/components/JoeQL.js
@@ -0,0 +1,44 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = transpile;
+var _graphql = require('graphql');
+
+function transpile(query) {
+  var operationName,
+      source = new _graphql.Source(query, 'GraphQL request'),
+      documentAST = (0, _graphql.parse)(source),
+      operationAST = (0, _graphql.getOperationAST)(documentAST, operationName);
+
+  function selectionSetVisitor(selectionSet) {
+    return selectionSet.selections.map(selectionVisitor);
+  }
+
+  function selectionVisitor(selection) {
+    return {
+      name: selection.name.value,
+      selectionSet: selection.selectionSet ? selectionSetVisitor(selection.selectionSet) : null
+    };
+  }
+
+  var slimTree = selectionSetVisitor(operationAST.selectionSet);
+
+  function traverseFields(selectionSet, wrap) {
+    var fields = selectionSet.map(function (selection) {
+      if (selection.selectionSet) return [selection.name, traverseFields(selection.selectionSet, true)];else return selection.name;
+    });
+
+    fields = fields.join(',');
+
+    if (wrap) return ('(' + fields + ')').replace(',(', '(');else return fields.replace(',(', '(');
+  }
+
+  var result = slimTree.map(function (st) {
+    var fields = traverseFields(st.selectionSet);
+    return st.name + '?fields=' + fields;
+  });
+
+  return result[0];
+}
